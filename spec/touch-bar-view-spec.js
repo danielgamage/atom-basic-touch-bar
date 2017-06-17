7401ec7d944485a42098a253a5a217fd4546bd67
Daniel Gamage <hello@danielgamage.com>
2017-06-17 17:22:42 -0400

    init

---
+++ spec/touch-bar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TouchBarView from '../lib/touch-bar-view';
+
+describe('TouchBarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
